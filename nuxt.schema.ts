367ffcdb03bf2b68f7f7fb98d793cc38e467dbb6
Christian <christian.palladini2@gmail.com>
2024-04-22 23:28:38 +0200

    feat: add nuxt.schema

---
+++ nuxt.schema.ts
@@ -0,0 +1,328 @@
+export default defineNuxtSchema({
+    appConfig: {
+      /**
+       * Theme configuration.
+       *
+       * @studioIcon material-symbols:docs
+       */
+      config: {
+        /**
+         * Website title, used as header default title and meta title.
+         *
+         * @studioIcon material-symbols:title
+         */
+        title: 'DocsMaker',
+        /**
+         * The website title template, to overwrite the default one.
+         */
+        titleTemplate: '%s · DocsMaker',
+        /**
+         * Website description, used for meta description.
+         *
+         * @studioIcon material-symbols:description
+         */
+        description: 'The best place to start your documentation.',
+        /**
+         * Cover image.
+         *
+         * @example '/cover.jpg'
+         *
+         * @studioIcon dashicons:cover-image
+         * @studioInput file
+         */
+        image: '',
+  
+        /**
+         * Social links
+         *
+         * Will be used in AppSocialIcons component.
+         *
+         * @studioIcon material-symbols:share-outline
+         */
+        socials: {
+          /**
+           * Twitter social handle
+           * @example 'nuxt_js'
+           * @studioIcon simple-icons:twitter
+           */
+          twitter: '',
+          /**
+           * GitHub org or repository
+           * @example 'nuxt/framework'
+           * @studioIcon simple-icons:github
+           */
+          github: '',
+          /**
+           * Facebook page url
+           * @example https://www.facebook.com/groups/nuxtjs
+           * @studioIcon simple-icons:facebook
+           */
+          facebook: '',
+          /**
+           * Instagram page url
+           * @example https://www.instagram.com/wearenuxt
+           * @studioIcon simple-icons:instagram
+           */
+          instagram: '',
+          /**
+           * Instagram page url
+           * @example https://www.youtube.com/@NuxtLabs
+           * @studioIcon simple-icons:youtube
+           */        
+          tiktok: '',
+          /**
+           * TikTok page url
+           * @example https://www.tiktok.com/@tiktok
+           * @studioIcon simple-icons:tiktok
+           */
+          youtube: '',
+          /**
+           * Medium page url
+           * @example https://medium.com/nuxt
+           * @studioIcon simple-icons:medium
+           */
+          medium: ''
+        },
+  
+        /**
+         * Theme layout configuration.
+         *
+         * @type {'default'|'page'}
+         * @studioIcon tabler:arrow-autofit-width
+         */
+        layout: 'default',
+  
+        /**
+         * Aside navigation configuration.
+         *
+         * @studioIcon fluent:document-page-24-regular
+         */
+        aside: {
+          /**
+           * Aside navigation level
+           *
+           * Use 0 to disable all nesting.
+           * Use 1 and more to display nested navigation in header and aside navigation.
+           */
+          level: 0,
+          /**
+           * Specify if default collapsibles state globally for aside navigation.
+           */
+          collapsed: false,
+          /**
+           * Paths to be excluded from aside navigation.
+           *
+           * @type {string[]}
+           */
+          exclude: []
+        },
+  
+        /**
+         * Header configuration.
+         *
+         * @studioIcon fluent:document-header-24-regular
+         */
+        header: {
+          /**
+           * Website title
+           *
+           * Title to be displayed in header or as aria-label if logo is defined.
+           *
+           * Default to config.title
+           *
+           * @studioIcon material-symbols:title
+           */
+          title: '',
+          /**
+           * Logo configuration
+           *
+           * Boolean to disable or use the `Logo.vue` component.
+           *
+           * String to be used as a name of a component.
+           * 
+           * Object that sets the light and dark logos.
+           *
+           * @type {boolean|string|{dark: string, light: string}}
+           * @example 'MyLogo'
+           * @studioInput boolean
+           */
+          logo: false,
+          /**
+           * Header links
+           *
+           * Toggle links icons in the header.
+           */
+          showLinkIcon: false,
+          /**
+           * Paths to be excluded from header links.
+           *
+           * @type {string[]}
+           *
+           */
+          exclude: [],
+          /**
+           * Makes the content of the header fluid.
+           */
+          fluid: false
+        },
+  
+        /**
+         * Main content configuration.
+         *
+         * @studioIcon fluent:document-header-footer-24-filled
+         */
+        main: {
+          /**
+           * Makes the content of the main container fluid.
+           */
+          fluid: false,
+          /**
+           * Makes the content of the main container padded.
+           */
+          padded: true
+        },
+  
+        /**
+         * Footer configuration
+         *
+         * @studioIcon fluent:document-footer-24-regular
+         */
+        footer: {
+          /**
+           * Website credits configuration.
+           *
+           * @type {false|{icon: string, text: string, href: string}}
+           *
+           * @studioIcon material-symbols:copyright
+           */
+          credits: {
+            /**
+             * Icon to show on credits
+             */
+            icon: '',
+            text: '',
+            href: ''
+          },
+          /**
+           * Text links
+           *
+           * Will be added into center section of the footer.
+           *
+           * @studioIcon material-symbols:add-link
+           */
+          textLinks: {
+            $schema: {
+              type: 'array',
+              items: {
+                type: 'object',
+                required: ['text', 'href'],
+                properties: {
+                  href: {
+                    type: 'string',
+                    description: 'URL when clicking the link'
+                  },
+                  text: {
+                    type: 'string',
+                    description: 'Text of the link'
+                  },
+                  target: {
+                    type: 'string',
+                    description: 'Target attribute of the link'
+                  },
+                  rel: {
+                    type: 'string',
+                    description: 'Rel attribute of the link'
+                  }
+                }
+              }
+            }
+          },
+          /**
+           * Icon links
+           *
+           * Icons to be added to Social Icons in footer.
+           *
+           * @studioIcon material-symbols:add-link
+           */
+          iconLinks: {
+            $schema: {
+              type: 'array',
+              items: {
+                type: 'object',
+                required: ['icon', 'href'],
+                properties: {
+                  icon: {
+                    type: 'string',
+                    description: 'Icon name'
+                  },
+                  href: {
+                    type: 'string',
+                    description: 'Link when clicking on the icon'
+                  },
+                  label: {
+                    type: 'string',
+                    description: 'Label of the icon'
+                  },
+                  rel: {
+                    type: 'string',
+                    description: 'Rel attribute of the link'
+                  }
+                }
+              }
+            }
+          },
+          /**
+           * Makes the content of the footer fluid.
+           */
+          fluid: true
+        },
+  
+        /**
+         * GitHub integration
+         *
+         * Configure the Edit on Github integration.
+         *
+         * @studioIcon simple-icons:github
+         */
+        github: {
+          /**
+           * Base URL
+           *
+           * You can use this to link to GitHub Enterprise.
+           */
+          baseUrl: 'https://github.com',
+          /**
+           * Directory
+           *
+           * Your GitHub repository root directory.
+           */
+          dir: '',
+          /**
+           * Branch
+           *
+           * Your GitHub repository branch.
+           */
+          branch: '',
+          /**
+           * Repository
+           *
+           * Your GitHub repository name.
+           */
+          repo: '',
+          /**
+           * Owner
+           *
+           * Your GitHub repository owner.
+           */
+          owner: '',
+          /**
+           * EditOnGithub
+           *
+           * Display EditOnGithub button.
+           */
+          edit: false
+        }
+      }
+    }
+  })
+  
