6fec18a91367dd842f4814ca9847648ecaed2c0e
Christian <christian.palladini2@gmail.com>
2024-04-22 12:43:42 +0200

    feat: add nuxt-color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content",  "@unocss/nuxt"]
+  modules: ["@nuxt/content",  "@unocss/nuxt", '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: '',
+  },
 })
