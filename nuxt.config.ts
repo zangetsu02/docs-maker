facdb7bdbe0485c01f3f9ccfd41cbbae183b03cc
Christian <christian.palladini2@gmail.com>
2024-04-22 22:55:32 +0200

    chore: add .npmrc, update nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,31 @@
+import { createResolver } from "nuxt/kit"
+
+const { resolve } = createResolver(import.meta.url)
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content",  "@unocss/nuxt", '@nuxtjs/color-mode'],
+  modules: [
+    "@nuxt/content",
+    "@unocss/nuxt",
+    "@nuxtjs/color-mode"
+  ],
   colorMode: {
     classSuffix: '',
   },
+  content: {
+    documentDriven: true,
+  },
+  components: [
+    {
+      prefix: '',
+      path: resolve('./components/app'),
+      global: true
+    },
+    {
+      prefix: '',
+      path: resolve('./components/docs'),
+      global: true
+    }
+  ],
 })
